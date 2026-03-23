9c72dbb0912431d23ec6a3eb7194cd9bed5ff78a
fennerys <fennerysseah@gmail.com>
2026-03-23 16:08:39 +0800

    wizard class with attributes

---
+++ Wizard.java
@@ -0,0 +1,13 @@
+public class Wizard extends Player
+{
+    Wizard()
+    {
+        super("Wizard", 200, 200, 50, 10, 20);
+    }
+
+    Wizard(String name)
+    {
+        super(name, 200, 200, 50, 10, 20);
+    }
+
+}
