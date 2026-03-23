6bfb8fabe327c6700e0c19bdaf6e97bed5ae8288
fennerys <fennerysseah@gmail.com>
2026-03-23 16:07:07 +0800

    Wizard class w attributes

---
+++ Wizard.java
@@ -0,0 +1,13 @@
+public class Warrior extends Player
+{
+    Warrior()
+    {
+        super("Warrior", 260, 260, 40, 20, 30);
+    }
+
+    Warrior(String name)
+    {
+        super(name, 260, 260, 40, 20, 30);
+    }
+    
+}
