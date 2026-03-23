bcdad3ac4b6dbe483686dc1b0c891415d5800a17
fennerys <fennerysseah@gmail.com>
2026-03-23 16:05:49 +0800

    Combatant abstract class

---
+++ Combatant.java
@@ -0,0 +1,34 @@
+public abstract class Combatant
+{
+    protected String name;
+    protected int maxHP;
+    protected int HP;
+    protected int ATK;
+    protected int DEF;
+    protected int SPD;
+    protected List<StatusEffect> StatusEffect;
+
+    public Combatant (String username, int maxHP, int HP, int ATK, int DEF, int SPD)
+    {
+        this.name = username;
+        this.maxHP = maxHP;
+        this.HP = HP;
+        this.ATK = ATK;
+        this.DEF = DEF;
+        this.SPD = SPD;
+    }
+
+    public void takeDamage (int dmg)
+    {
+        this.HP = this.HP - dmg;
+        if (this.HP < 0)
+        {
+            this.HP = 0;
+        }
+    }
+
+    public boolean isDefeated()
+    {
+        return (this.HP <= 0); 
+    }
+}
