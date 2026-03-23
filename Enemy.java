64448afa290b22f1c433aa6c63c469a6613a01f3
fennerys <fennerysseah@gmail.com>
2026-03-23 16:28:22 +0800

    Create abstract Enemy class extending Combatant

---
+++ Enemy.java
@@ -0,0 +1,7 @@
+abstract class Enemy extends Combatant
+{
+    public Enemy(String name, int maxHP, int HP, int ATK, int DEF, int SPD)
+    {
+        super(name, maxHP, HP, ATK, DEF, SPD);
+    }
+} 
