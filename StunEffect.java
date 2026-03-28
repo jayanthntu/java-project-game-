e93eaaf376e544a6a880d6cd7357e2c723911591
dream rae yap <dreamrae.yap@gmail.com>
2026-03-28 11:24:13 +0800

    Implemented getName()

--- StunEffect.java
+++ StunEffect.java
@@ -2,13 +2,20 @@ public class StunEffect extends StatusEffect {
     public StunEffect(int duration) {
         super(duration);
     }
+
     @Override
     public void apply(Combatant target) {
         target.setStunned(true);
     }
+
     @Override
     public void remove(Combatant target) {
         target.setStunned(false);
+    }
+
+    @Override
+    public String getName() {
+        return "Stun effect";
     }
 }
 
