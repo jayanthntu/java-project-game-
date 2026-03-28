ce9c7f204acec6faea881ba0e4acbcd97239595d
dream rae yap <dreamrae.yap@gmail.com>
2026-03-28 11:09:49 +0800

    Made constructor public

--- ItemAction.java
+++ ItemAction.java
@@ -1,9 +1,10 @@
 public class ItemAction implements Action {
     private Item itemToUse;
 
-    private ItemAction(Item selectedItem) {
+    public ItemAction(Item selectedItem) {
         this.itemToUse = selectedItem;
     }
+
     @Override
     public void execute(Combatant user, Combatant target) {
         itemToUse.use(user, target);
