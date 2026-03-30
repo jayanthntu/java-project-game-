5d851b5169b64b9586fb229f52592cb664a9c90f
Marc Teh <marcteh@gmail.com>
2026-03-30 15:15:40 +0800

    Removed CombatArena folder

--- CombatArena/src/action/Action.java
+++
@@ -1,8 +0,0 @@
-package action;
-
-import combatant.Combatant;
-import java.util.List;
-
-public interface Action {
-    void execute(Combatant actor, List<Combatant> targets);
-}
--- CombatArena/src/action/ActionStrategy.java
+++
@@ -1,7 +0,0 @@
-package action;
-
-public class ActionStrategy {
-    public Action selectAction() {
-        return new BasicAttack();
-    }
-}
