04ed7adc3752f995fdb406fa0db4670dc26f8a35
axmeliaa <amel0031@e.ntu.edu.sg>
2026-03-30 07:14:13 +0000

    hcsjjsk

--- effect/ShieldBash.java
+++ effect/ShieldBash.java
@@ -1,7 +1,7 @@
 public class ShieldBash implements Action {
     @Override
     public void execute(Combatant attacker, Combatant target) {
-        int dmg = Math.max(0, attacker.getAttack() - target.getDefense());
+        int dmg = attacker.getAttack() - target.getDefense();
         target.takeDamage(dmg);
         target.addStatusEffect(new StunEffect(2));
         attacker.setSpecialSkillCooldown(3);
