e900e75beb27dbd147a41205a1ea4abd25a23117
axmeliaa <amel0031@e.ntu.edu.sg>
2026-03-30 07:08:28 +0000

    bcvsji

---
+++ BasicAttack.java
@@ -0,0 +1,7 @@
+public class BasicAttack implements Action {
+    @Override
+    public void execute(Combatant attacker, Combatant target) {
+        int dmg =  attacker.getAttack() - target.getDefense();
+        target.takeDamage(dmg);
+    }
+}
