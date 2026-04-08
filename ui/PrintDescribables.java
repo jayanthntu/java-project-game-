f969c6c789f9bddadcfe71a5116a53e447a2284a
Puff Nuff <FennerysSeah@gmail.com>
2026-04-08 17:50:47 +0800

    created printdescribables class to print all describables to improve extensibility

---
+++ ui/PrintDescribables.java
@@ -0,0 +1,18 @@
+package ui;
+
+import java.util.List;
+
+public class PrintDescribables 
+{
+
+    public static <T extends Describable> void printDescribables(String title, List<T> list) 
+    {
+        System.out.println("\n=== " + title.toUpperCase() + " ===");
+
+        for (int i = 0; i < list.size(); i++) 
+        {
+            T obj = list.get(i);
+            System.out.println((i + 1) + ". " + obj.getName() + " - " + obj.getDescription());
+        }
+    }
+}
