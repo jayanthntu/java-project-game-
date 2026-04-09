2b16710d15beca206cd20594f21a6acb1080bbaa
dream rae yap <dreamrae.yap@gmail.com>
2026-04-09 21:33:53 +0800

    Changed attacker to user for consistency

--- action/BasicAttack.java
+++ action/BasicAttack.java
@@ -6,19 +6,19 @@ import java.util.List;
 
 public class BasicAttack implements Action {
     @Override
-    public void execute(Combatant attacker, List<Combatant> targets) {
+    public void execute(Combatant user, List<Combatant> targets) {
         Combatant target = targets.getFirst();
 
         if (!target.isInvulnerable()) {
-            int dmg = Math.max(0, attacker.getAttack() - target.getDefense());
-            System.out.println(attacker.getName() + " attacks " + target.getName()
-                    + "! Damage: " + attacker.getAttack() + " - "
+            int dmg = Math.max(0, user.getAttack() - target.getDefense());
+            System.out.println(user.getName() + " attacks " + target.getName()
+                    + "! Damage: " + user.getAttack() + " - "
                     + target.getDefense() + " = " + dmg);
             target.takeDamage(dmg);
             System.out.println(target.getName() + " HP: "
                     + target.getHP() + "/" + target.getMaxHP());
         } else {
-            System.out.println(attacker.getName() + " attacks " + target.getName()
+            System.out.println(user.getName() + " attacks " + target.getName()
                     + "! Damage: 0 (Smoke bomb active)");
         }
     }
--- action/ShieldBash.java
+++ action/ShieldBash.java
@@ -11,12 +11,12 @@ public class ShieldBash implements SpecialSkill {
         Selected enemy is unable to take actions for the current turn and the next turn.""";
 
     @Override
-    public void execute(Combatant attacker, List<Combatant> targets) {
+    public void execute(Combatant user, List<Combatant> targets) {
         Combatant target = targets.getFirst();
-        int dmg = Math.max(0, attacker.getAttack() - target.getDefense());
+        int dmg = Math.max(0, user.getAttack() - target.getDefense());
         target.takeDamage(dmg);
         target.addStatusEffect(new StunEffect(2));
-        attacker.setSpecialSkillsCooldown(3);
+        user.setSpecialSkillsCooldown(3);
     }
 
     @Override
