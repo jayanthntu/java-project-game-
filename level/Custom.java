b694c134695d3fcea2efc7185223d6e3e0f78936
dream rae yap <dreamrae.yap@gmail.com>
2026-04-10 10:52:24 +0800

    Fixed custom difficulty creation abstraction

--- level/Custom.java
+++ level/Custom.java
@@ -1,13 +1,14 @@
 package level;
 
 import combatant.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Custom implements Difficulty {
     private static final String NAME = "Custom";
     private static final String DESCRIPTION = "? Goblins + ? Wolves | Backup: ? Goblins + ? Wolves";
-    private static final List<Enemy> initialSpawn = List.of(new Goblin("Goblin A"), new Goblin("Goblin B"), new Goblin("Goblin C"));
-    private static final List<Enemy> backupSpawn = List.of();
+    private static final List<Enemy> initialSpawn = new ArrayList<>();
+    private static final List<Enemy> backupSpawn = new ArrayList<>();
 
     @Override
     public String getName() {
@@ -27,5 +28,15 @@ public class Custom implements Difficulty {
     @Override
     public List<Enemy> getBackupSpawn() {
         return backupSpawn;
+    }
+
+    public void setInitialSpawn(int initGoblins, int initWolves) {
+        for (int i = 0; i < initGoblins; i++) initialSpawn.add(new Goblin("Goblin " + (char)('A' + i)));
+        for (int i = 0; i < initWolves; i++) initialSpawn.add(new Wolf("Wolf "   + (char)('A' + i)));
+    }
+
+    public void setBackupSpawn(int backupGoblins, int backupWolves) {
+        for (int i = 0; i < backupGoblins; i++) backupSpawn.add(new Goblin("Goblin " + (char)('A' + i)));
+        for (int i = 0; i < backupWolves; i++) backupSpawn.add(new Wolf("Wolf "   + (char)('A' + i)));
     }
 }
--- level/LevelFactory.java
+++ level/LevelFactory.java
@@ -1,8 +1,5 @@
 package level;
 
-import combatant.*;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class LevelFactory {
@@ -15,20 +12,17 @@ public class LevelFactory {
         };
         return new Level(difficultyType, difficulty.getInitialSpawn(), difficulty.getBackupSpawn());
     }
+
     public static Level createCustom(List<Integer> customSettings) {
         int initGoblins  = customSettings.get(0);
         int initWolves   = customSettings.get(1);
         int backupGoblins = customSettings.get(2);
         int backupWolves  = customSettings.get(3);
 
-        List<Enemy> initialSpawns = new ArrayList<>();
-        List<Enemy> backupSpawns  = new ArrayList<>();
+        Custom difficulty = new Custom();
+        difficulty.setInitialSpawn(initGoblins, initWolves);
+        difficulty.setBackupSpawn(backupGoblins, backupWolves);
 
-        for (int i = 0; i < initGoblins; i++) initialSpawns.add(new Goblin("Goblin " + (char)('A' + i)));
-        for (int i = 0; i < initWolves; i++) initialSpawns.add(new Wolf("Wolf "   + (char)('A' + i)));
-        for (int i = 0; i < backupGoblins; i++) backupSpawns.add(new Goblin("Goblin " + (char)('A' + i)));
-        for (int i = 0; i < backupWolves; i++) backupSpawns.add(new Wolf("Wolf "    + (char)('A' + i)));
-
-        return new Level(Difficulty.TYPE.CUSTOM, initialSpawns, backupSpawns);
+        return new Level(Difficulty.TYPE.CUSTOM, difficulty.getInitialSpawn(), difficulty.getBackupSpawn());
     }
 }
